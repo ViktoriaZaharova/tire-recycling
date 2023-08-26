5d482591dccd0e0956c145626c521ad464b2b95b
Виктория Захарова <zaharova181114@gmail.com>
2023-08-26 13:33:13 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$('[name="phone"]').mask('+7 (999) 999-99-99');
+
+$('.go_to').click(function (e) {
+	e.preventDefault();
+	var scroll_el = $(this).attr('href');
+	if ($(scroll_el).length !== 0) {
+		$('html, body').animate({
+			scrollTop: $(scroll_el).offset().top
+		}, 500);
+	}
+	return false;
+});
