b5aad4e11c015a1ffedfcbfb1310f9469e2634a2
Виктория Захарова <zaharova181114@gmail.com>
2023-08-28 22:07:49 +0300

    edit1

--- js/common.js
+++ js/common.js
@@ -11,3 +11,16 @@ $('.go_to').click(function (e) {
 	}
 	return false;
 });
+
+$(document).ready(function () {
+	$(".js-tab-trigger").click(function () {
+		var id = $(this).attr('data-tab'),
+			content = $('.js-tab-content[data-tab="' + id + '"]');
+
+		$('.mega-menu__route.active').removeClass('active'); // 1
+		$(this).addClass('active'); // 2
+
+		$('.js-tab-content.active').removeClass('active'); // 3
+		content.addClass('active'); // 4
+	})
+});
